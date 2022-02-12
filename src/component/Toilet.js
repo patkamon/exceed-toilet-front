50a11a98ee92d5b1207923d32acf3ab4c45262d2
pattanan-cyber <78087419+pattanan-cyber@users.noreply.github.com>
2022-02-12 14:26:53 +0700

    change directory, change image and some design

--- src/component/Toilet.js
+++ src/component/Toilet.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import './toilet.css';
 
 import { GrRotateRight } from 'react-icons/gr';
 
@@ -14,8 +14,80 @@ const Toilet = () => {
     <div>Toilet
 
 
-<h1>Hi</h1>
+
  <button><GrRotateRight /></button> 
+
+{/* 
+      <h1>Toilet</h1>
+      <h1>Average estimated time: HH:MM</h1>
+      <div class="toilet-card">
+          <div class="card-header"></div>
+          <div class="card-body">
+              <img src="https://cdn.discordapp.com/attachments/941214299160993822/941945726156554290/toilet-5.png" alt="image" />
+              <h3 class="toilet-room">Room1</h3>
+              <p class="time-display">Begin time: HH.MM</p>
+              <p class="estimatetime-display">Estimated end time: HH.MM</p>
+          </div>
+      </div>
+      <div class="toilet-card">
+          <div class="card-header"></div>
+          <div class="card-body">
+              <img src="https://cdn.discordapp.com/attachments/941214299160993822/941945725875539968/toilet-4.png" alt="image" />
+              <h3 class="toilet-room">Room2</h3>
+              <p class="empty-room">Empty!</p>
+              <p class="estimatetime-display">Estimated end time: HH.MM</p>
+          </div>
+      </div>
+      <div class="toilet-card">
+          <div class="card-header"></div>
+          <div class="card-body">
+              <img src="https://cdn.discordapp.com/attachments/941214299160993822/941945726156554290/toilet-5.png" alt="image" />
+              <h3 class="toilet-room">Room3</h3>
+              <p class="time-display">Begin time: HH.MM</p>
+              <p class="estimatetime-display">Estimated end time: HH.MM</p>
+          </div>
+      </div> */}
+
+
+
+  <h1>TOILET</h1>
+
+  {/* <h1>Average estimated time: HH:MM</h1> */}
+  <div class="toilet-list">
+      
+      <div class="toilet-card">
+          <div class="card-header"></div>
+          <div class="card-body">
+              <img src="https://cdn.discordapp.com/attachments/941214299160993822/941954703003123742/toilet-6.png" alt="image" />
+              <h3 class="toilet-room">ROOM 1</h3>
+              <p class="time-display">DO NOT DISTURB</p>
+              <p class="time-display">Begin time: HH.MM</p>
+              <p class="estimatetime-display">Estimated end time: HH.MM</p>
+          </div>
+      </div>
+      <div class="toilet-card">
+          <div class="card-header"></div>
+          <div class="card-body">
+              <img src="https://cdn.discordapp.com/attachments/941214299160993822/941954703284113408/toilet-7.png" alt="image" />
+              <h3 class="toilet-room">ROOM 2</h3>
+              <p class="empty-room">EMPTY ROOM</p>
+              <p class="estimatetime-display">Estimated end time: HH.MM</p>
+          </div>
+      </div>
+      <div class="toilet-card">
+          <div class="card-header"></div>
+          <div class="card-body">
+              <img src="https://cdn.discordapp.com/attachments/941214299160993822/941954703003123742/toilet-6.png" alt="image" />
+              <h3 class="toilet-room">ROOM 3</h3>
+              <p class="time-display">DO NOT DISTURB</p>
+              <p class="time-display">Begin time: HH.MM</p>
+              <p class="estimatetime-display">Estimated end time: HH.MM</p>
+          </div>
+      </div>
+  </div>
+  
+
+
 
     </div>
 
