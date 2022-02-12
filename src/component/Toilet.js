6af81c2799932a7f277d72274d132da25a290cba
patkamon <patkamon.a@ku.th>
2022-02-12 13:10:56 +0700

    add component

---
+++ src/component/Toilet.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+
+import { GrRotateRight } from 'react-icons/gr';
+
+const Toilet = () => {
+
+
+
+
+
+
+  return (
+    <div>Toilet
+
+
+<h1>Hi</h1>
+ <button><GrRotateRight /></button> 
+
+    </div>
+
+
+    
+  )
+}
+
+export default Toilet
